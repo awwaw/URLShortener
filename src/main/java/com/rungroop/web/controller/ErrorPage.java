6672698ca67d3b99db7ea04bf2261d07144b6fab
aww <vovapodkorytov966@gmail.com>
2024-08-01 21:42:13 +0300

    ErrorPage fixed

--- src/main/java/com/rungroop/web/controller/ErrorPage.java
+++ src/main/java/com/rungroop/web/controller/ErrorPage.java
@@ -8,16 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ErrorPage {
-    @GetMapping("{anything}")
-    public String getDefaultErrorPage(Model model, @PathVariable("anything") final String path) {
-        if (path.equals("error_page")) {
-            return "redirect:/error_page";
-        }
-        model.addAttribute("errorMessage", "This page does not exist");
-        return "ErrorPage";
-    }
-
-    @GetMapping("error_page")
+    @GetMapping({"error_page", "{ignored}"})
     public String getErrorPage(Model model, @ModelAttribute("errorMessage") String errorMessage) {
         if (errorMessage.isEmpty()) {
             errorMessage = "This page does not exist";
